31c585f2e4e407edbecc00b6a4830dae9d13ad4c
Daniel Gyorffy <judalion777@gmail.com>
2022-12-20 12:50:00 +0100

    Add solution template

---
+++ Renewed/megoldasok.sql
@@ -0,0 +1,23 @@
+-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+-- 1. feladat:
+
+-- 3. feladat:
+
+-- 4. feladat:
+
+-- 5. feladat:
+
+-- 6. feladat:
+
+-- 7. feladat:
+
+-- 8. feladat:
+
+-- 9. feladat:
+
+-- 10. feladat:
+
+-- 11. feladat:
+
+-- 12. feladat:
